c64f1e322fa1403e4f9e17eb5c4f2471eb99f13d
Matthew LaSpina <matthew.laspina@cellsignal.com>
2023-04-05 14:35:24 -0400

    Main.go update

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprint(w, "Hello, GitOps!")
     })
+    http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "Hello, User!")
+    })
 
     http.ListenAndServe(":8080", nil)
 }
-
